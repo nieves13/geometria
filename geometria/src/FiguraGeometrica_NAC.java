daeaaa76c5a7eb85a3db057c0d3d128b636436d1
Nieves Aracil Cerdan <nievesaracil@gmail.com>
2020-02-03 08:45:56 +0100

    version refactorizada

--- geometria/src/FiguraGeometrica_NAC.java
+++ geometria/src/FiguraGeometrica_NAC.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_NAC {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_NAC(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
--- geometria/src/Rectangulo_NAC.java
+++ geometria/src/Rectangulo_NAC.java
@@ -1,13 +1,9 @@
-/**
- * 
- * @author Nieves
- *
- */
-public class Rectangulo extends FiguraGeometrica {
+
+public class Rectangulo_NAC extends FiguraGeometrica_NAC {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo(String tipoFigura, double lG, double lP) {
+	public Rectangulo_NAC(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
