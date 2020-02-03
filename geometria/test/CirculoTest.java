9e7fcc9b53956b7ad01b5005219b36c2f5f8d383
Nieves Aracil Cerdan <nievesaracil@gmail.com>
2020-02-03 09:09:12 +0100

    Uff ahora sí

---
+++ geometria/test/CirculoTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import hola.Circulo_NAC;
+
+public class CirculoTest {
+
+	@Test
+	public void testArea() {
+		Circulo_NAC c1 = new Circulo_NAC(7,"Circulo");
+		double res = c1.area();
+		assertEquals(153.9384,res,0.0001);
+		Circulo_NAC c2 = new Circulo_NAC(0,"Circulo");
+		double res2 = c2.area();
+		assertEquals(0,res2,0.0001);
+		Circulo_NAC c3 = new Circulo_NAC(-3,"Circulo");
+		double res3 = c3.area();
+		assertEquals(28.2744,res3,0.0001);
+	}
+
+	@Test
+	public void testPerimetro() {
+		Circulo_NAC c1 = new Circulo_NAC(7,"Circulo");
+		double res = c1.perimetro();
+		assertEquals(43.9824,res,0.0001);
+		Circulo_NAC c2 = new Circulo_NAC(0,"Circulo");
+		double res2 = c2.perimetro();
+		assertEquals(0,res2,0.0001);
+		Circulo_NAC c3 = new Circulo_NAC(-3,"Circulo");
+		double res3 = c3.perimetro();
+		assertEquals(18.8496,res3,0.0001);
+	}
+
+}
