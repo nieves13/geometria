46637d3b53673d1899eb600637067ea845b1a997
Nieves Aracil Cerdan <nievesaracil@gmail.com>
2020-02-03 08:54:05 +0100

    versión comentada

--- geometria/src/Rectangulo_NAC.java
+++ geometria/src/Rectangulo_NAC.java
@@ -1,19 +1,37 @@
-
+/**
+ * Clase que extiende la clase abstracta Figura geometrica y realiza un rectangulo
+ * @author Nieves Aracil Cerdán
+ * @version 1.2
+ */
 public class Rectangulo_NAC extends FiguraGeometrica_NAC {
-	private double l1;
-	private double l2;
+	private double l1; //lado del rectángulo
+	private double l2; //lado del rectángulo
 	
+	/**
+	 * Constructor del Rectangulo
+	 * @param tipoFigura Indica el tipo de figura llamando a la clase FiguraGeometrica_NAC
+	 * @param lG Introduce la anchura de uno de los lados
+	 * @param lP Introduce la anchura de uno de los lados
+	 */
 	public Rectangulo_NAC(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+	/**
+	 * Calcula el área del rectángulo
+	 * @return Devuelve el resultado de multiplicar los lados.
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+	/**
+	 * Calcula el perímetro del rectángulo
+	 * @return Devuelve el resultado de la operación que calcula el perímetro del rectángulo.
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
